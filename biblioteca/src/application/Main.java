8138a1dde972e2edf018b729c6221e91d2b1c308
VirsoRaivoky <wilson_xavier@hotmail.com.br>
2023-08-11 08:34:47 -0300

    Adicinado Resourcers

--- biblioteca/src/application/Main.java
+++ biblioteca/src/application/Main.java
@@ -6,12 +6,21 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Objects;
+
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+
 		try {
-            Pane root = FXMLLoader.load(getClass().getResource("/view/FXMLMain.fxml"));
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("bibliotecaDB");
+			EntityManager em = emf.createEntityManager();
+            Pane root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/FXMLMain.fxml")));
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -22,5 +31,7 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+		//EntityManagerFactory emf = Persistence.createEntityManagerFactory("bibliotecaDB");
+		//EntityManager em = emf.createEntityManager();
 	}
 }
